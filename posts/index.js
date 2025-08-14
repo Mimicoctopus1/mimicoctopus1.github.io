13b1b35da15a956f89ef685beb61ec812b8d229c
lemondelightstand <lemondelightstand@gmail.com>
2025-08-14 19:45:48 +0000

    Changed public/posts to organize by the date the post was written vs the date the event in the post occured. Also added a thicker border around the entire table.

--- posts/index.js
+++ posts/index.js
@@ -5,7 +5,7 @@ while(postsLeft > 0) {
   let post = posts[postsLeft - 1]
   let date = post.children[0].innerHTML.split("/")
 
-  post.children[1].innerHTML = "<a href=\"/posts/" + date[2] + "/" + date[0] + "/" + date[1] + "/" + post.children[1].innerHTML + "\">" + post.children[1].innerHTML + "</a>"
+  post.children[2].innerHTML = "<a href=\"/posts/" + date[2] + "/" + date[0] + "/" + date[1] + "/" + post.children[2].innerHTML + "\">" + post.children[2].innerHTML + "</a>"
   
   postsLeft--
 }
