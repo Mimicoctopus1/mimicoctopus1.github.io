a5163fd6e966af48b6e4d81e46460be04466317d
odiemo000 <odiemo000@gmail.com>
2025-10-29 19:06:27 +0000

    ✨Added universal.css and made a population sign that shows the current number of users using the newly installed ws module.

--- index.js
+++ index.js
@@ -7,14 +7,30 @@ var app = express()
 var server = http.createServer(app)
 var wss = new WebSocket.WebSocketServer({"server": server})
 
-app.use(express.static("./public"))/*Allow the user to access the public folder.*/
+app.use(express.static(path.join(__dirname, "public")))/*Allow the user to access the public folder.*/
 app.get('/', (request, response) => {/*When the user requests the domain root...*/
-  response.render("./public/index.html")/*Send the index.html file.*/
+  response.render(path.join(__dirname, "public", "index.html"))/*Send the index.html file.*/
 })
 
 var port = parseInt(process.env.PORT) || process.argv[3] || 8080;
 server.listen(port)
 
+wss.send = function(data) {
+  wss.clients.forEach(function(ws) {
+    ws.send(data)
+  })
+}
+
 wss.on("connection", function(ws) {
-  console.log("User connected " + ws)
+  wss.send(JSON.stringify({
+    "type": "population update",
+    "data": wss.clients.size
+  }))
+  
+  ws.on("close", function(ws) {
+    wss.send(JSON.stringify({
+      "type": "population update",
+      "data": wss.clients.size
+    }))
+  })
 })
