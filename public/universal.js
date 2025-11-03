151ff29ebf2d92b18a7d7a9ecc8420787555d248
odiemo000 <odiemo000@gmail.com>
2025-11-03 21:09:19 +0000

    🎨Fixed indentation in injector.

--- public/universal.js
+++ public/universal.js
@@ -4,40 +4,40 @@ var head = document.querySelectorAll("head")[0]
 var style = document.createElement("style")
 style.innerHTML = `
 .header {
-line-height: 1em;
-background-color: rgba(255, 255, 255, 1);
-border: 1px solid rgba(0, 0, 0, 1);
-padding: none;
-position: fixed;
-top: 0;
-left: 0;
-width: 100%;
-height: 10vh;
-overflow-x: auto;
-overflow-y: hidden;
-z-index: 100;
+  line-height: 1em;
+  background-color: rgba(255, 255, 255, 1);
+  border: 1px solid rgba(0, 0, 0, 1);
+  padding: none;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 10vh;
+  overflow-x: auto;
+  overflow-y: hidden;
+  z-index: 100;
 }
 
 .header div {
-display: inline-block;
-text-align: center;
-height: 100%;
+  display: inline-block;
+  text-align: center;
+  height: 100%;
 }
 
 .header div a {
-display: block;
-text-decoration: none;
-height: 100%;
-font-size: 3vh;
+  display: block;
+  text-decoration: none;
+  height: 100%;
+  font-size: 3vh;
 }
 
 .header div a img {
-height: 65%;/*The text if supposed to take up 3% of the viewport height, 30% of the menubar. The image takes up almost the other 70%.*/
+  height: 65%;/*The text if supposed to take up 3% of the viewport height, 30% of the menubar. The image takes up almost the other 70%.*/
 }
 
 body {
-margin-top: 0;
-padding-top: 10vh;/*Makes room for the menubar.*/
+  margin-top: 0;
+  padding-top: 10vh;/*Makes room for the menubar.*/
 }
 `
 
@@ -69,28 +69,28 @@ footer.className = "footer"
 footer.innerHTML = `
 <h1>Acknowledgements</h1>
 <div class="acknowledgements">
-<div>
+  <div>
     <h2>Domain (<a href="https://199.k.vu">199.k.vu</a>)</h2>
     <a href="https://freedns.afraid.org">FreeDNS</a>
-</div>
-<div>
+  </div>
+  <div>
     <h2>Static Web Hosting</h2>
     <a href="https://docs.github.com/en/pages">GitHub Pages</a><br>
     <a href="https://codeberg.page">Codeberg Pages</a>
-</div>
-<div>
+  </div>
+  <div>
     <h2>Code Hosting</h2>
     <a href="https://github.com/home">GitHub</a><br>
     <a href="https://codeberg.org/about">Codeberg</a>
-</div>
-<div>
+  </div>
+  <div>
     <h2>IDE <span class="headerdescription">(Integrated Development Environment)</span></h2>
     <a href="https://firebase.studio">Firebase Studio</a>
-</div>
-<div>
+  </div>
+  <div>
     <h2>Image Editing</h2>
     <a href="https://wickeditor.com">The Wick Editor</a>
-</div>
+  </div>
 </div>
 `
 
