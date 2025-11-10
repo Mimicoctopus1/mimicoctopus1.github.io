667c1f9caa83d6bd57ed9f142b58b119cec574b4
odiemo000 <odiemo000@gmail.com>
2025-11-10 23:20:57 +0000

    ⚡️Universalized client-side WebSocket message handling.

--- index.js
+++ index.js
@@ -1,13 +1,7 @@
 var populationPlaceholder = document.getElementsByClassName("populationPlaceholder")[0]
 
-var messageResponses = {
+Object.assign(ws.responses, {
 	"population update": function(data) {
 		populationPlaceholder.innerHTML = "<h1><div>This Website<br>POP.: " + data + "</div></h1>"
 	}
-}
-
-ws.addEventListener("message", function(event) {
-	let type = JSON.parse(event.data).type
-	let data = JSON.parse(event.data).data
-	messageResponses[type](data)
 })
