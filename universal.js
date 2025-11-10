c6682d24a16ff590627b5171e8b344985c11aff0
odiemo000 <odiemo000@gmail.com>
2025-11-10 23:20:57 +0000

    ⚡️Universalized client-side WebSocket message handling.

--- universal.js
+++ universal.js
@@ -1,1 +1,11 @@
 ws = new WebSocket(document.location)
+
+ws.responses = {}
+
+ws.addEventListener("message", function(event) {
+	let type = JSON.parse(event.data).type
+	let data = JSON.parse(event.data).data
+	if(ws.responses.type) {
+		ws.responses[type](data)
+	}
+})
